3921a8a617b0160b75c060fa97399497b84487d8
malums1232002-dotcom <malums1232002@gmail.com>
2026-06-10 06:48:02 +0000

    Update Javascript practice files

---
+++ StringMethods.js
@@ -0,0 +1,15 @@
+//string length
+let name="Maluma";
+console.log(name.length);
+
+//slice
+let name1="MalathiRavi";
+console.log(name1.slice(0,5));
+
+//replace
+let sentence="i am a girl";
+console.log(sentence.replace("girl","boy"));
+
+//includes
+let word="animals";
+console.log(word.includes("kio"));
